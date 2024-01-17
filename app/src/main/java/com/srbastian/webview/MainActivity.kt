6441e2452b19d37d1f6110f5517e49463f1c3eff
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-01-17 13:14:22 -0500

    Add some comments to the MainActivity.

--- app/src/main/java/com/srbastian/webview/MainActivity.kt
+++ app/src/main/java/com/srbastian/webview/MainActivity.kt
@@ -10,7 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // * Well at here the application doesn't function like we mean.
+        // * cuz we need to change the manifest file to give internet access to our app
+        // * So we go a to change that
         webView = findViewById(R.id.web)
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://github.com/SrbastianM")
